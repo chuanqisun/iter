541648264aaf55880f48bd533e158eaddbacfb53
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2025-01-20 10:46:14 -0800

    move life cycle hook into artifact

--- src/artifact/artifact.ts
+++ src/artifact/artifact.ts
@@ -6,9 +6,10 @@ import { bundledLanguages, createHighlighter } from "shiki/bundle/web";
 import { GenericArtifact } from "./languages/generic";
 import { MermaidArtifact } from "./languages/mermaid";
 import { ScriptArtifact } from "./languages/script";
-import { type ArtifactSupport } from "./languages/type";
+import { type ArtifactContext, type ArtifactSupport } from "./languages/type";
 import { XmlArtifact } from "./languages/xml";
 
+import type { CodeEditorElement } from "../code-editor/code-editor-element";
 import "./artifact.css";
 
 export const supportedLanguages = Object.keys(bundledLanguages);
@@ -107,12 +108,12 @@ export function handleArtifactActions(event: MouseEvent) {
 
       if (isEditing) {
         artifactElement.removeEventListener("rerun", handleRerun);
-        artifact.onRunExit?.({ lang, code, trigger });
-        artifact.onEditExit({ lang, code, trigger });
+        onRunExit?.({ lang, code, trigger });
+        onEditExit({ lang, code, trigger });
       } else {
         artifactElement.querySelector("dialog")?.showModal();
         artifactElement.addEventListener("rerun", handleRerun);
-        artifact.onEdit({ lang, code, trigger });
+        onEdit({ lang, code, trigger });
         artifact.onRun?.({ lang, code, trigger });
       }
       return;
@@ -128,4 +129,71 @@ export function handleArtifactActions(event: MouseEvent) {
       return;
     }
   }
+}
+
+function onEdit({ trigger, code, lang }: ArtifactContext) {
+  const artifactElement = trigger.closest("artifact-element")!;
+  const focusTrapElement = artifactElement.querySelector("artifact-focus-trap-element")!;
+
+  trigger.classList.add("running");
+  trigger.textContent = "Back";
+  const editorContainer = artifactElement.querySelector("artifact-edit")!;
+  const editor = document.createElement("code-editor-element") as CodeEditorElement;
+  editor.setAttribute("data-lang", lang);
+  editor.setAttribute("data-value", code);
+  editor.setAttribute("data-autofocus", "");
+  editorContainer.appendChild(editor);
+
+  // editor node will be removed, no need to remove listeneres
+  editor.addEventListener("contentchange", () => {
+    const latestSourceCode = editor.value;
+    artifactElement.querySelector("artifact-source")!.textContent = latestSourceCode;
+    artifactElement.dispatchEvent(new CustomEvent("rerun", { detail: latestSourceCode }));
+  });
+
+  editor.addEventListener("run", (e) => {
+    const latestSourceCode = (e as CustomEvent<string>).detail;
+    artifactElement.dispatchEvent(new CustomEvent("rerun", { detail: latestSourceCode }));
+  });
+
+  // first escape: exit editor focus capture, allow tab movement
+  // second escape: exit edit mode
+  editor.addEventListener("escape", () => editor.setAttribute("data-readonly", ""));
+  editor.addEventListener("escapereadonly", () => onEditExit({ trigger, code, lang }));
+  editor.addEventListener("enterreadonly", () => editor.removeAttribute("data-readonly"));
+
+  editor.addEventListener("mousedown", (_e) => {
+    if (editor.hasAttribute("data-readonly")) {
+      editor.removeAttribute("data-readonly");
+    }
+  });
+
+  focusTrapElement.toggleAttribute("disabled", false);
+}
+
+function onEditExit({ trigger, code }: ArtifactContext) {
+  const artifactElement = trigger.closest("artifact-element")!;
+  const focusTrapElement = artifactElement.querySelector("artifact-focus-trap-element")!;
+
+  trigger.classList.remove("running");
+  trigger.textContent = "Edit";
+  const editor = artifactElement.querySelector<CodeEditorElement>("code-editor-element")!;
+  const latestSourceCode = editor.value;
+  const allArtifacts = [...artifactElement.parentElement!.querySelectorAll("artifact-element")];
+  const index = allArtifacts.indexOf(artifactElement);
+  artifactElement
+    .closest(".js-message")
+    ?.querySelector("code-block-events")
+    ?.dispatchEvent(new CustomEvent("codeblockchange", { detail: { index, prev: code, current: latestSourceCode } }));
+  editor.remove();
+
+  focusTrapElement.toggleAttribute("disabled", true);
+}
+
+function onRunExit({ trigger }: ArtifactContext) {
+  const renderContainer = trigger.closest("artifact-element")?.querySelector<HTMLElement>("artifact-preview");
+  if (!renderContainer) return;
+
+  trigger.classList.remove("running");
+  renderContainer.innerHTML = "";
 }
--- src/artifact/languages/type.ts
+++ src/artifact/languages/type.ts
@@ -1,11 +1,8 @@
 export interface ArtifactSupport {
   /** From md code block language to shiki highlight language */
   onResolveLanguage: (lang: string) => string | undefined;
-  onEdit: ArtifactHandler;
-  onEditExit: ArtifactHandler;
   onCopy: ArtifactHandler;
   onRun?: ArtifactHandler;
-  onRunExit?: ArtifactHandler;
   onSave: ArtifactHandler;
 }
 
