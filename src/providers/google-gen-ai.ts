6029f28486e74c989ec4abaef997fd8937cdbedd
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-06-05 17:07:09 -0700

    google model upgrade

--- src/providers/google-gen-ai.ts
+++ src/providers/google-gen-ai.ts
@@ -29,7 +29,7 @@ export interface GoogleGenAIConnection extends BaseConnection {
 export class GoogleGenAIProvider implements BaseProvider {
   static type = "google-gen-ai";
   static defaultModels = [
-    "gemini-2.5-pro-preview-05-06",
+    "gemini-2.5-pro-preview-06-05",
     "gemini-2.5-flash-preview-05-20",
     "gemini-2.5-pro-exp-03-25",
     "gemini-2.0-pro-exp-02-05",
